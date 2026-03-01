f66a3c03f4da0e63ef037ce4d170d8d56112b027
satishlakamsani <lakamsanisatish@gmail.com>
2026-03-01 11:02:57 +0530

    add aws_vpc

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = local.vpc_final_tags
+}
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id  #vpc association
+
+  tags = local.igw_final_tags
+}
+
+#public subnets
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+
+    local.common_tags,
+  {
+    Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+  },
+  var.public_subnet_tags
+
+  )
+}
---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "project"{
+    type = string
+}
+
+variable "environment"{
+    type =string
+}
+
+variable "vpc_cidr"{
+    type = string
+    default = "0.0.0.0/16"
+
+}
+
+variable "vpc_tags"{
+    type = map 
+    default = {}
+}
+
+variable "igw_tags"{
+    type = map
+    default ={}
+}
+
+variable "public_subnet_cidrs"{
+    type = list
+    default =  ["10.0.1.0/24","10.0.2.0/24"]
+
+}
+
+variable "public_subnet_tags"{
+    default = {}
+    type = map
+}
